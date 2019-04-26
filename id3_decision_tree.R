3a93f6566c56b1e1ad7053e7518bf04e2ab64721
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2019-04-26 19:17:29 -0300

    implementando

--- id3_decision_tree.R
+++ id3_decision_tree.R
@@ -18,18 +18,29 @@ randomColor <- function(){
   paste("#", new_color[1], new_color[2], new_color[3], sep = "")
 }
 
+argmax <- function(df){
+  which.max(df)
+}
+
 entropy <-function(label_col){
   probs_den <- nrow(label_col)
   probs <- as.vector(table(label_col)) / probs_den
   -sum(probs * log2(probs))
 }
 
-filter_df <- function(df, col_name, col_val) {
-  df[which(df[col_name] == col_val),]
+filter_df <- function(col_val, col_name, label_col, df) {
+  filtered_df <- df[which(df[col_name] == col_val),]
+  filtered_df <- filtered_df[label_col]
 }
 
-information_gain <- function(df, col_name, label_col){
+information_gain <- function(col_name, label_col, df){
   df_E <- entropy(df[label_col])
+  probs_den <- nrow(df)
+  probs <- as.vector(table(df[col_name])) / probs_den
+  col_vals <- names(table(df[col_name]))
+  filtered_df <- lapply(col_vals, filter_df, col_name = col_name, label_col = label_col, df = df)
+  col_E <- unlist(lapply(filtered_df, entropy))
+  df_E - sum(probs * col_E)
 }
 
 #############################################################################
@@ -44,19 +55,13 @@ base_df <- read_csv("datasets/play_tennis.csv")[-1]
 ####                            TESTING AREA                             ####
 #############################################################################
 
+label_col <- "play_tennis"
 df <- base_df
 
-col_name <- "wind"
-label_col <- "play_tennis"
+col_names <- names(df[, -which(names(df) == label_col)])
+igains <- lapply(col_names, information_gain, label_col = label_col, df = df)
 
-df_E <- entropy(df[label_col])
-probs_den <- nrow(df)
-probs <- as.vector(table(df[col_name])) / probs_den
-col_vals <- names(table(df[col_name]))
-
-filter_ds <- apply(ds, filter_col, ...)
-
-unq_vals <- unique(base_df[col_name])
-for(condition in unique(base_df[col_name])){
-  print(un)
+vertex_fifo <- c()
+if(any(igains > 0.0)){
+  append(vertex_fifo, col_names[argmax(igains)])
 }
